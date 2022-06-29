3829690faa9cbee04a158d03c2bc2017ed78d919
Keval Shah <kevalshah278@gmail.com>
2022-06-29 11:57:17 -0400

    added comments, might have template issues

---
+++ model_data/Robot.h
@@ -0,0 +1,35 @@
+/*
+Robot class with all robot model parameters
+A single parametric constructor for data memebers
+*/
+#ifndef _ROBOT_
+#define _ROBOT_
+
+template <class T>
+class Robot
+{
+    public:
+        // Parametric Constructor to initialize robot parameters
+        Robot(T modelParameters);
+
+    private:
+        T wheelToCenter;
+        T wheelRadius;
+        T gearBoxReduction;
+        /*
+        Dynamic Model and Motor Parameters
+        Check the report for description of each parameter
+        */
+        T Kv;
+        T R;
+        T M;
+        T J;
+        T Bv;
+        T Bvn;
+        T Bw;
+        T Cv;
+        T Cvn;
+        T Cw;
+};
+
+#endif
