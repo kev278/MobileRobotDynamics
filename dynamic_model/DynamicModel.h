6f6c3928f931eaf0adbf84b367c384194fa51983
Keval Shah <kevalshah278@gmail.com>
2022-06-29 14:36:41 -0400

    initial commit, pending structure of class

---
+++ dynamic_model/DynamicModel.h
@@ -0,0 +1,16 @@
+#ifndef _DYNAMICMODEL_
+#define _DYNAMICMODEL_
+
+#include "KinematicModel.h"
+
+template<class T>
+class DynamicModel : public KinematicModel
+{
+    public:
+    
+
+    private:
+
+};
+
+#endif
