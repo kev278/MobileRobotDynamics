d30461821e7a2e49fb35fa31a99c137e2dc0d1f2
Keval Shah <kevalshah278@gmail.com>
2022-07-14 11:31:57 -0400

    tested KM class

--- model/main.cpp
+++ model/main.cpp
@@ -1,9 +1,10 @@
-#include "Robot.h"
+#include "KinematicModel.h"
 #include "Data.h"
 #include <iostream>
 
 int main()
 {
+
     // Task 1: Load data from CSV
     Data data;
     data.getFileContent();
@@ -14,5 +15,9 @@ int main()
     std::vector<double> modelParams = data.getParams();
     Robot robot(modelParams);
     std::cout << "Check 2" << std::endl;
+
+    // Task 3: Initialize Kinematic Model
+    KinematicModel kinematicModel;
+    std::cout << "Check 3" << std::endl;
 
 }
