95f4719582e3102af007a75c2a70771d80d4d76b
Keval Shah <kevalshah278@gmail.com>
2022-07-14 17:39:18 -0400

    added a calcVel function

--- model/KinematicModel.cpp
+++ model/KinematicModel.cpp
@@ -1,16 +1,26 @@
-#include <KinematicModel.h>
-#include <Eigen/Dense>
+#ifndef _KMCPP_
+#define _KMCPP_
+
+#include "KinematicModel.h"
+#include "Eigen/Dense"
 #include <cmath>
+#include <iostream>
 
 using KM = KinematicModel;
 
-// Solve this error
-void KM::calcLinearVel(double wheelToCenter, double velRobot, double velNormalRobot, double omegaRobot)
+KM::KinematicModel(std::vector<double> modelParams)
+: Robot(modelParams), linVel0(0), linVel1(0), linVel2(0), angVel0(0), angVel1(0), angVel2(0)
+{
+    
+}
+
+//void KM::calcLinearVel(double wheelToCenter, double velRobot, double velNormalRobot, double omegaRobot)
+void KM::calcLinearVel(double velRobot, double velNormalRobot, double omegaRobot)
 {
     Eigen::MatrixXd Rotation(3, 3);
-    Rotation << -sqrt(3) / 2, 1 / 2, wheelToCenter,
-                 0,          -1,     wheelToCenter,
-                 sqrt(3) / 2, 1 / 2, wheelToCenter;
+    Rotation << -sqrt(3) / 2, 1 / 2, Robot::wheelToCenter,
+                 0,          -1,     Robot::wheelToCenter,
+                 sqrt(3) / 2, 1 / 2, Robot::wheelToCenter;
 
     Eigen::MatrixXd stateVec(3, 1);
     stateVec << velRobot, velNormalRobot, omegaRobot;
@@ -21,9 +31,17 @@ void KM::calcLinearVel(double wheelToCenter, double velRobot, double velNormalRo
     KM::linVel2 = wheelLinearVel(3, 1);
 }
 
-void KM::calAngularVel(double wheelRadius)
+void KM::calcAngularVel()
 {
-    KM::angVel0 = KM::linVel0 / wheelRadius;
-    KM::angVel1 = KM::linVel1 / wheelRadius;
-    KM::angVel2 = KM::linVel2 / wheelRadius;
+    KM::angVel0 = KM::linVel0 / Robot::wheelRadius;
+    KM::angVel1 = KM::linVel1 / Robot::wheelRadius;
+    KM::angVel2 = KM::linVel2 / Robot::wheelRadius;
 }
+
+void KM::calcVel(double velRobot, double velNormalRobot, double omegaRobot)
+{
+    calcLinearVel(velRobot, velNormalRobot, omegaRobot);
+    calcAngularVel();
+}
+
+#endif // _KMCPP_
