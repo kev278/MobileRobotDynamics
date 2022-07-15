8fae0171acb79bb65a978d69e1998dc3ff2fb71c
Keval Shah <kevalshah278@gmail.com>
2022-07-14 23:56:36 -0400

    data members and methods added

--- model/DynamicModel.h
+++ model/DynamicModel.h
@@ -6,10 +6,17 @@
 class DynamicModel : public KinematicModel
 {
     public:
-    
+        DynamicModel(std::vector<double> modelParams);
+        Eigen::MatrixXd calcStateDot(double velRobot, double velNormalRobot, double omegaRobot, double u1, double u2, double u3);
+        
 
     private:
-
+        double sign_func(double x);
+        double accV;
+        double accVn;
+        double accAng;
 };
+
+#include "DynamicModel.cpp"
 
 #endif
