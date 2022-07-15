7d669a7e588cd23838b0f1a32c05f91b6cf41af5
Keval Shah <kevalshah278@gmail.com>
2022-07-14 23:58:38 -0400

    dynamic model & traj test

--- model/main.cpp
+++ model/main.cpp
@@ -1,6 +1,8 @@
 #include "KinematicModel.h"
+#include "Trajectory.h"
 #include "Data.h"
 #include <iostream>
+#include "DynamicModel.h"
 
 int main()
 {
@@ -10,14 +12,27 @@ int main()
     data.getFileContent();
     data.setModelParameters();
     std::cout << "Check 1" << std::endl;
-
-    // Task 2: Initialize a Robot
     std::vector<double> modelParams = data.getParams();
 
     // Task 3: Initialize Kinematic Model
     KinematicModel kinematicModel(modelParams);
     std::cout << "Check 2" << std::endl;
 
-    // Task 4: Calcuate Velocities
-    
+    // Task 3: Calcuate Velocities
+    kinematicModel.calcVel(1, 1, 1);
+    std::cout << "Check 3" << std::endl;
+
+    // Task 4: Initialize Dynamic Model
+    DynamicModel dynamicModel(modelParams);
+    std::cout << "Check 4" << std::endl;
+
+    // Task 5: Input Velocity Trajectory to get Acceleration Trajectory
+    Trajectory trajectory;
+    trajectory.getFileContent();
+    trajectory.setTraj();
+    std::vector<double> velX = trajectory.getX();
+    std::vector<double> velY = trajectory.getY();
+    std::vector<double> omega = trajectory.getOmega();
+    std::cout << "Check 5" << std::endl;
+
 }
