4305b0577b6a3aa22f710057f5711b94a1fe443b
Keval Shah <kevalshah278@gmail.com>
2022-07-16 17:20:05 -0400

    added size args for Eigen matrices

--- model/DynamicModel.cpp
+++ model/DynamicModel.cpp
@@ -13,7 +13,7 @@ DynamicModel::DynamicModel(std::vector<double> modelParams)
 
 }
 
-Eigen::MatrixXd DynamicModel::calcStateDot(double velRobot, double velNormalRobot, double omegaRobot, double u1, double u2, double u3)
+void DynamicModel::calcStateDot(double velRobot, double velNormalRobot, double omegaRobot, double u1, double u2, double u3)
 {
     /* TODO
     Add Kt to model parameters
@@ -25,15 +25,15 @@ Eigen::MatrixXd DynamicModel::calcStateDot(double velRobot, double velNormalRobo
     double A22 = - (3 * pow(Kt, 2) * pow(gearBoxReduction, 2)) / (2 *  pow(wheelRadius, 2) * R * M) - Bvn / M; 
     double A33 = - (3 *  pow(wheelToCenter, 2) * pow(Kt, 2) * pow(gearBoxReduction, 2)) / (pow(wheelRadius, 2) * R * J) - Bw / J; 
 
-    Eigen::MatrixXd A;
+    Eigen::MatrixXd A(3, 3);
     A << A11, 0,   0,
          0,   A22, 0,
          0,   0,   A33;
 
-    Eigen::MatrixXd stateVec;
+    Eigen::MatrixXd stateVec(3, 1);
     stateVec << velRobot, velNormalRobot, omegaRobot;
 
-    Eigen::MatrixXd B;
+    Eigen::MatrixXd B(3, 3);
     B << -sqrt(3) / (2 * M), 0,                 sqrt(3) / (2 * M),
           1 / (2 * M), 1 /   M,                 1 / (2 * M),
           wheelToCenter / J, wheelToCenter / J, wheelToCenter / J;
@@ -41,23 +41,32 @@ Eigen::MatrixXd DynamicModel::calcStateDot(double velRobot, double velNormalRobo
     double lambda = (gearBoxReduction * Kt) / (wheelRadius * R);
     B *= lambda;
 
-    Eigen::MatrixXd U;
+    Eigen::MatrixXd U(3, 1);
     U << u1, u2, u3;
 
-    Eigen::MatrixXd K;
+    Eigen::MatrixXd K(3, 3);
     K << -Cv / M, 0,       0,
           0,     -Cvn / M, 0,
           0,      0,      -Cw / J;
 
-    Eigen::MatrixXd signX;
+    Eigen::MatrixXd signX(3, 1);
     x1 = sign_func(velRobot);
     x2 = sign_func(velNormalRobot);
     x3 = sign_func(omegaRobot);
     signX << x1, x2, x3;
 
 
-    Eigen::MatrixXd stateVecDot;
+    Eigen::MatrixXd stateVecDot(3, 1);
     stateVecDot = A * stateVec + B * U + K * signX;
+    accV = stateVecDot.coeff(1, 1);
+    accVn = stateVecDot.coeff(2, 1);
+    accAng = stateVecDot.coeff(3, 1);
+
+}
+
+inline std::vector<double> DynamicModel::getStateVecDot()
+{
+    std::vector<double> stateVecDot {accV, accVn, accAng};
     return stateVecDot;
 }
 
